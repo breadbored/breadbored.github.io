41abb7a8e7c2728e9d807c74ced28038bec0e350
Jason Carpenter <5916026+breadbored@users.noreply.github.com>
2025-06-11 23:50:36 -0400

    Deploy to gh-pages

---
+++ _next/static/ys4-awy3T1wYF2Oipi-sA/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Farchive","\u002Farchive\u002F[slug]","\u002Fbsky-test","\u002Fposts\u002F[slug]","\u002Fvolos-guide-to-monsters"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
